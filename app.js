c18a8469105d86873a398f99ba1bb6ee6597c306
jorge <jorgelopezgonzalez474@gmail.com>
2020-10-29 12:33:18 -0300

    Add 404 page

--- app.js
+++ app.js
@@ -19,4 +19,8 @@ const shopRoutes = require("./routes/shop");
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>404</h1>");
+});
+
 app.listen(4000);
