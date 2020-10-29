ea06f851ab309ba561eabb4c40b116dd1be0061c
jorge <jorgelopezgonzalez474@gmail.com>
2020-10-29 17:15:32 -0300

    Add styles

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "/views", "/404.html"));
+  res.status(404).sendFile(path.join(rootDir, "/views", "/404.html"));
 });
 
 app.listen(4000);
