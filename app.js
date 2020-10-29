aa29a87b254942e2b2efa5983a73969457954ebe
jorge <jorgelopezgonzalez474@gmail.com>
2020-10-29 16:19:19 -0300

    Add page 404.html

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
+const path = require("path"); //Con path puedo obtener la ruta donde estoy y de esta forma me sirva para todos los SO
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -20,7 +22,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>404</h1>");
+  res.status(404).sendFile(path.join(__dirname, "/views", "/404.html"));
 });
 
 app.listen(4000);
